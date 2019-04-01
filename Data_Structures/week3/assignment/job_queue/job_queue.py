cbcb733341984181424667e9eb85c16ee1353a76
Yuna Luzi <junaluzi@gmail.com>
2019-03-31 21:34:37 -0400

    basic implementation of job_queue but requires debugging

--- Data_Structures/week3/assignment/job_queue/job_queue.py
+++ Data_Structures/week3/assignment/job_queue/job_queue.py
@@ -21,34 +21,41 @@ class JobQueue:
 	def rightchild(self, i):
 		return 2*i + 2
 
-	def siftDown(self, i):
+	def siftDown(self, i, array):
+		''' double check to see if it works for this case '''
 		left_index = self.leftchild(i)
 		right_index = self.rightchild(i)
-		size = len(self.jobs)
+		size = len(array)
 		min_index = i
 		if left_index < size and right_index < size: # node has left and right child
 			# compare which of children nodes is smallest
-			if self.jobs[left_index] < self.jobs[right_index]:
+			if array[left_index] < array[right_index]:
 				min_index = left_index
 			else:
 				min_index = right_index
-			if self.jobs[min_index] < self.jobs[i]: # swap
-				self.jobs[min_index], self.jobs[i] = self.jobs[i], self.jobs[min_index]
+			if array[min_index] < array[i]: # swap
+				array[min_index], array[i] = array[i], array[min_index]
 				self.siftDown(min_index)
 		elif left_index == size - 1: # node has only left child
-			if self.jobs[left_index] < self.jobs[i]: # swap
+			if array[left_index] < array[i]: # swap
 				min_index = left_index
-				self.jobs[left_index], self.jobs[i] = self.jobs[i], self.jobs[left_index]
+				array[left_index], array[i] = array[i], array[left_index]
 				self.siftDown(min_index)
 
-	def extractMin(self):
-		if len(self.jobs) > 0:
-			self.jobs[0], self.jobs[len(self.jobs)-1] = self.jobs[len(self.jobs)-1], self.jobs[0]
-			result = self.jobs.pop()
-			self.siftDown(0)
-			return result
-		return False
+	def siftUp(self, i):
+		''' basic idea - implement correctly '''
+		while i > 1 and array[self.parent(i)] < array[i]:
+			array[self.parent(i)], array[i] = array[i], array[self.parent(i)]
+			i = self.parent(i)
 
+	def changePriority(self, i, priority):
+		''' basic idea - implement correctly '''
+		old_priority = array[i]
+		array[i] = priority
+		if priority > old_priority:
+			self.siftUp(i)
+		else:
+			self.siftDown(i)
 
 	def assign_jobs(self):
 		# TODO: replace this code with a faster algorithm.
@@ -64,23 +71,9 @@ class JobQueue:
 		# 	self.start_times[i] = next_free_time[next_worker]
 		# 	next_free_time[next_worker] += self.jobs[i]
 
-		## create a heap with number of m threads
-		self.
-
-		self.start_times = []
-		self.assigned_workers = []
-
-		start_time = 0
-
-		while self.jobs: # while there are still jobs to process
-			for i in range(self.num_workers):
-				result = self.extractMin()
-				#self.assigned_workers.append(i)
-				#self.start_times.append(start_time)
-				print(i, start_time)
-				start_time = 
-				
-
+		# create a heap with number of m threads
+		for i in range(self.num_workers):
+			print(i)
 
 	def solve(self):
 		self.read_data()
