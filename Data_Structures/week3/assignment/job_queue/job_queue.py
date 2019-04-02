e728e0a3a3a68bc75329c8794faadf06b1d7d7c2
Yuna Luzi <junaluzi@gmail.com>
2019-04-01 20:41:45 -0400

    week 3 assignment 2 fails on #28

--- Data_Structures/week3/assignment/job_queue/job_queue.py
+++ Data_Structures/week3/assignment/job_queue/job_queue.py
@@ -4,81 +4,103 @@ class JobQueue:
 	def read_data(self):
 		# self.num_workers, m = map(int, input().split())
 		# self.jobs = list(map(int, input().split()))
-		self.num_workers, m = 2, 5
-		self.jobs = [1, 2, 3, 4, 5]
+		self.num_workers, m = 14, 3
+		self.jobs = [2,11,6]
 		assert m == len(self.jobs)
 
 	def write_response(self):
 		for i in range(len(self.jobs)):
 			print(self.assigned_workers[i], self.start_times[i])
 
-	def parent(self, i):
-		return int( (i+1) / 2)
+	def parent(self, k):
+		return int( (k+1) / 2)
 
-	def leftchild(self, i):
-		return 2*i + 1
+	def leftchild(self, k):
+		return 2*k + 1
 
-	def rightchild(self, i):
-		return 2*i + 2
+	def rightchild(self, k):
+		return 2*k + 2
 
-	def siftDown(self, i, array):
-		''' double check to see if it works for this case '''
-		left_index = self.leftchild(i)
-		right_index = self.rightchild(i)
-		size = len(array)
-		min_index = i
-		if left_index < size and right_index < size: # node has left and right child
-			# compare which of children nodes is smallest
-			if array[left_index] < array[right_index]:
-				min_index = left_index
+	def getMin(self):
+		return self.heap[0]
+
+	def siftDown(self, k):
+		''' implement correctly '''
+		minIndex = k
+		leftIndex = self.leftchild(k)
+		rightIndex = self.rightchild(k)
+		size = len(self.heap)
+
+		if leftIndex < size and rightIndex < size: # if left + right children
+			''' checks left and right which is bigger '''
+			left = self.heap[leftIndex]
+			right = self.heap[rightIndex]
+			if left[1] < right[1]:
+				minIndex = leftIndex
+			elif left[1] == right[1]: # choose one with smallest id
+				if left[0] < right[0]:
+					minIndex = leftIndex
+				else:
+					minIndex = rightIndex
 			else:
-				min_index = right_index
-			if array[min_index] < array[i]: # swap
-				array[min_index], array[i] = array[i], array[min_index]
-				self.siftDown(min_index)
-		elif left_index == size - 1: # node has only left child
-			if array[left_index] < array[i]: # swap
-				min_index = left_index
-				array[left_index], array[i] = array[i], array[left_index]
-				self.siftDown(min_index)
+				minIndex = rightIndex
+			''' checks node with whichever child is bigger '''
+			childMin = self.heap[minIndex]
+			current = self.heap[k]
+			if childMin[1] < current[1]:
+				# swap and siftdown
+				self.heap[minIndex], self.heap[k] = self.heap[k], self.heap[minIndex]
+				self.siftDown(minIndex)
+			elif childMin[1] == current[1]:
+				if childMin[0] < current[0]:
+					self.heap[minIndex], self.heap[k] = self.heap[k], self.heap[minIndex]
+					self.siftDown(minIndex)
+		elif leftIndex == size - 1: # node has only left child
+			left = self.heap[leftIndex]
+			current = self.heap[k]
+			if left[1] < current[1]:
+				self.heap[leftIndex], self.heap[k] = self.heap[k], self.heap[leftIndex]
+				self.siftDown(leftIndex)
+			elif left[1] == current[1]:
+				if left[1] < current[0]:
+					self.heap[leftIndex], self.heap[k] = self.heap[k], self.heap[leftIndex]
+					self.siftDown(leftIndex)
 
-	def siftUp(self, i):
+	def siftUp(self, k):
 		''' basic idea - implement correctly '''
-		while i > 1 and array[self.parent(i)] < array[i]:
-			array[self.parent(i)], array[i] = array[i], array[self.parent(i)]
-			i = self.parent(i)
+		while k > 1 and self.heap[self.parent(k)] < self.heap[k]:
+			self.heap[self.parent(k)], self.heap[k] = self.heap[k], self.heap[self.parent(k)]
+			k = self.parent(k)
 
-	def changePriority(self, i, priority):
-		''' basic idea - implement correctly '''
-		old_priority = array[i]
-		array[i] = priority
-		if priority > old_priority:
-			self.siftUp(i)
+	def changePriority(self, k, priority):
+		''' old priority is wrong '''
+		old_priority = self.heap[k][1]
+		self.heap[k][1] = priority
+		if priority < old_priority:
+			self.siftUp(k)
 		else:
-			self.siftDown(i)
+			self.siftDown(k)
 
 	def assign_jobs(self):
-		# TODO: replace this code with a faster algorithm.
-		# self.assigned_workers = [None] * len(self.jobs)
-		# self.start_times = [None] * len(self.jobs)
-		# next_free_time = [0] * self.num_workers
-		# for i in range(len(self.jobs)):
-		# 	next_worker = 0
-		# 	for j in range(self.num_workers):
-		# 	if next_free_time[j] < next_free_time[next_worker]:
-		# 		next_worker = j
-		# 	self.assigned_workers[i] = next_worker
-		# 	self.start_times[i] = next_free_time[next_worker]
-		# 	next_free_time[next_worker] += self.jobs[i]
+		self.assigned_workers = [None] * len(self.jobs)
+		self.start_times = [None] * len(self.jobs)
 
-		# create a heap with number of m threads
+		self.heap = [] # first is id, next is priority 
 		for i in range(self.num_workers):
-			print(i)
+			self.heap.append([i, 0])
+
+		for i in range(len(self.jobs)):
+			node = self.getMin() # always gets min value of priority
+			id_ = node[0]
+			result = node[1]
+			self.assigned_workers[i] = id_
+			self.start_times[i] = result
+			self.changePriority(0, result + self.jobs[i]) # id of node , result
 
 	def solve(self):
 		self.read_data()
 		self.assign_jobs()
-		#self.write_response()
+		self.write_response()
 
 if __name__ == '__main__':
 	job_queue = JobQueue()
