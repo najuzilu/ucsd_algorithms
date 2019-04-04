8ec8d67d22d10684800e334511ccde9da09f54c4
Yuna Luzi <junaluzi@gmail.com>
2019-04-04 19:20:15 -0400

    week 3 assignment 3 failed on case 13

--- Data_Structures/week3/assignment/merging_tables/merging_tables.py
+++ Data_Structures/week3/assignment/merging_tables/merging_tables.py
@@ -4,28 +4,32 @@ import sys
 
 n, m = map(int, sys.stdin.readline().split())
 lines = list(map(int, sys.stdin.readline().split()))
-rank = [1] * n
-parent = list(range(0, n))
-ans = max(lines)
 
-def getParent(table):
-    # find parent and compress path
-    return parent[table]
+rank = [0] * n
+parent = list(range(0, n))
+
+def getParent(entry):
+    if entry != parent[entry]:
+        parent[entry] = getParent(parent[entry])
+    return parent[entry]
 
 def merge(destination, source):
-    realDestination, realSource = getParent(destination), getParent(source)
+    input1, input2 = getParent(destination), getParent(source)
 
-    if realDestination == realSource:
+    if input1 == input2:
         return False
 
-    # merge two components
-    # use union by rank heuristic 
-    # update ans with the new maximum table size
-    
+    if rank[input1] > rank[input2]:
+        parent[input2] = input1
+        lines[input1] += lines[source]
+    else:
+        parent[input1] = input2
+        lines[input2] += lines[input1]
+        if rank[input1] == rank[input2]:
+            rank[input2] += 1
     return True
 
 for i in range(m):
     destination, source = map(int, sys.stdin.readline().split())
     merge(destination - 1, source - 1)
-    print(ans)
-    
+    print(max(lines))
