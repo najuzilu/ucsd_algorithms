aaca2c8052152483bd85c5f6e37f7e0811d8c178
Yuna Luzi <junaluzi@gmail.com>
2019-04-07 12:46:20 -0400

    hash chains assignment week 4 done

--- Data_Structures/week4/assignment/hash_chains/hash_chains.py
+++ Data_Structures/week4/assignment/hash_chains/hash_chains.py
@@ -9,15 +9,13 @@ class Query:
         else:
             self.s = query[1]
 
-
 class QueryProcessor:
     _multiplier = 263
     _prime = 1000000007
 
     def __init__(self, bucket_count):
         self.bucket_count = bucket_count
-        # store all strings in one list
-        self.elems = []
+        self.hashTable = [[] for i in range(self.bucket_count)]
 
     def _hash_func(self, s):
         ans = 0
@@ -35,23 +33,25 @@ class QueryProcessor:
         return Query(input().split())
 
     def process_query(self, query):
-        if query.type == "check":
-            # use reverse order, because we append strings to the end
-            self.write_chain(cur for cur in reversed(self.elems)
-                        if self._hash_func(cur) == query.ind)
-        else:
-            try:
-                ind = self.elems.index(query.s)
-            except ValueError:
-                ind = -1
-            if query.type == 'find':
-                self.write_search_result(ind != -1)
-            elif query.type == 'add':
-                if ind == -1:
-                    self.elems.append(query.s)
+        if query.type == 'check':
+            chain = self.hashTable[query.ind]
+            if chain == []:
+                print()
             else:
-                if ind != -1:
-                    self.elems.pop(ind)
+                self.write_chain(reversed(chain))
+        else:
+            index = self._hash_func(query.s)
+            if query.type == 'add':
+                if query.s not in self.hashTable[index]:
+                    self.hashTable[index].append(query.s)
+            elif query.type == 'find':
+                response = query.s in self.hashTable[index]
+                self.write_search_result(response)
+            elif query.type == 'del':
+                try:
+                    self.hashTable[index].remove(query.s)
+                except ValueError:
+                    pass
 
     def process_queries(self):
         n = int(input())
