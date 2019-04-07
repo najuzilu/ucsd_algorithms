2c4c3a74b0dc29ae409b9645af0bc802f7995b68
Yuna Luzi <junaluzi@gmail.com>
2019-04-06 20:45:40 -0400

    max pairwise product done; week 1 done

--- Algorithmic_Toolbox/week1/assignment/2_maximum_pairwise_product/max_pairwise_product.py
+++ Algorithmic_Toolbox/week1/assignment/2_maximum_pairwise_product/max_pairwise_product.py
@@ -1,18 +1,29 @@
 # python3
 
-
 def max_pairwise_product(numbers):
-    n = len(numbers)
-    max_product = 0
-    for first in range(n):
-        for second in range(first + 1, n):
-            max_product = max(max_product,
-                numbers[first] * numbers[second])
+	n = len(numbers)
+	max_product = 0
 
-    return max_product
+	firstMax = 0
+	for i in range(len(numbers)):
+		if numbers[i] > firstMax:
+			firstMax = numbers[i]
+			numbers[0], numbers[i] = numbers[i], numbers[0] # swap
+
+	secondMax = 0
+	for j in range(1, len(numbers)):
+		if numbers[j] > secondMax:
+			secondMax = numbers[j]
+			numbers[1], numbers[j] = numbers[j], numbers[1] # swap
+
+	max_product = firstMax * secondMax
+	return max_product
+
+def even_faster_max_pairwise_product(numbers):
+	return
 
 
 if __name__ == '__main__':
-    input_n = int(input())
-    input_numbers = [int(x) for x in input().split()]
-    print(max_pairwise_product(input_numbers))
+	input_n = int(input())
+	input_numbers = [int(x) for x in input().split()]
+	print(max_pairwise_product(input_numbers))
