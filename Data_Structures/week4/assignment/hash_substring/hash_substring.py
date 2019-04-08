7759ec4e3b50fe89f2053ec3a515196b4130987c
Yuna Luzi <junaluzi@gmail.com>
2019-04-08 10:26:57 -0400

    changed loop line 59 to string iteration with == pattern to reduce time from 9.99 to 0.79 / 5.00

--- Data_Structures/week4/assignment/hash_substring/hash_substring.py
+++ Data_Structures/week4/assignment/hash_substring/hash_substring.py
@@ -8,12 +8,6 @@ def read_input():
 def print_occurrences(output):
     print(' '.join(map(str, output)))
 
-def areEqual(string1, string2):
-	for i in range(len(string1) - 1):
-		if string1[i] != string2[i]:
-			return False 
-	return True
-
 def polyHash(pattern, prime, x):
 	'''
 		prime is int greater than 10**8
@@ -31,18 +25,20 @@ def precomputeHashes(text, len_pattern, prime, x):
 		prime is int greater than 10**8
 		x is a random int between [1, prime - 1]
 	'''
+	# Step 1: calculate TMiusP
+	TMiusP = len(text) - len_pattern
 	# Step 2: create the answer array with the length of the difference + 1
-	H = [None] * (len(text) - len_pattern + 1)
+	H = [None] * (TMiusP + 1)
 	# Step 3: get string which contains only the "last string"
-	S = text[len(text) - len_pattern:]
+	S = text[TMiusP:] # ONE SUBSTRING
 	# Step 4: generate the polyHash of the last entry
-	H[len(text) - len_pattern] = polyHash(S, prime, x)
+	H[TMiusP] = polyHash(S, prime, x)
 	# Step 5: create y = x**|P|
 	y = 1
 	for i in range(len_pattern):
 		y = (y * x) % prime
 	# Step 6: create result array
-	for i in range(len(text) - len_pattern - 1, -1, -1):
+	for i in range(TMiusP - 1, -1, -1):
 		#H[i] = ( x * H[i+1] + ord(text[i]) - y * ord(text[i+len_pattern]) + prime ) % prime
 		H[i] = (x * H[i + 1] + ord(text[i]) - y * ord(text[i + len_pattern]) + prime) % prime
 	return H
@@ -59,10 +55,10 @@ def get_occurrences(pattern, text):
 	H = precomputeHashes(text, len(pattern), prime, x)
 	# Step 5: check to see if hashes are the same and if they are the same whether it is a false positive
 	for i in range(len(text) - len(pattern) + 1):
-		if pHash == H[i] and areEqual(text[i:i+len(pattern)-1], pattern):
-			result.append(i)
+		if pHash == H[i]:
+			if text[i: i + len(pattern)] == pattern:
+				result.append(i)
 	return result
-	
 
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
