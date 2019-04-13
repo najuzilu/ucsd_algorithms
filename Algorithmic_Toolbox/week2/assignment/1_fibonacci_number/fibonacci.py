41a8e9edcbb7ebd9b8693026c2970f9cf909ef30
Yuna Luzi <junaluzi@gmail.com>
2019-04-13 08:21:48 -0400

    week 2 assignment fibonacci done

--- Algorithmic_Toolbox/week2/assignment/1_fibonacci_number/fibonacci.py
+++ Algorithmic_Toolbox/week2/assignment/1_fibonacci_number/fibonacci.py
@@ -1,9 +1,13 @@
 # Uses python3
 def calc_fib(n):
-    if (n <= 1):
-        return n
-
-    return calc_fib(n - 1) + calc_fib(n - 2)
+	array = [None] * (n + 1)
+	if n == 0:
+		return 0
+	array[0] = 0
+	array[1] = 1
+	for i in range(2, n + 1):
+		array[i] = array[i - 1] + array[i - 2]
+	return array[n]
 
 n = int(input())
 print(calc_fib(n))
