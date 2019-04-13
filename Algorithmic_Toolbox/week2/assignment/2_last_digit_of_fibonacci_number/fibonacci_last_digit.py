53a3fdf9cde39e3eb6d71c87dcea7aee44511ee9
Yuna Luzi <junaluzi@gmail.com>
2019-04-13 08:38:20 -0400

    week 2 assignment fibonacci last digit done

--- Algorithmic_Toolbox/week2/assignment/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
+++ Algorithmic_Toolbox/week2/assignment/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
@@ -2,18 +2,21 @@
 import sys
 
 def get_fibonacci_last_digit_naive(n):
-    if n <= 1:
+    array = [None] * (n + 1)
+
+    if n < 1:
         return n
 
-    previous = 0
-    current  = 1
+    array[0] = 0
+    array[1] = 1
 
-    for _ in range(n - 1):
-        previous, current = current, previous + current
 
-    return current % 10
+    for i in range(2, n + 1):
+        array[i] = (array[i - 1] + array[i - 2]) % 10
+    return array[n]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n = int(input)
+    # n = int(input())
     print(get_fibonacci_last_digit_naive(n))
