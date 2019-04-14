8770b9ed23d0afae4138c050e4948fca0a4979ba
Yuna Luzi <junaluzi@gmail.com>
2019-04-14 12:01:16 -0400

    week 2 assignment part 5 done - i think but no confirmation yet from the system

--- Algorithmic_Toolbox/week2/assignment/5_fibonacci_number_again/fibonacci_huge.py
+++ Algorithmic_Toolbox/week2/assignment/5_fibonacci_number_again/fibonacci_huge.py
@@ -1,19 +1,23 @@
 # Uses python3
 import sys
 
-def get_fibonacci_huge_naive(n, m):
-    if n <= 1:
-        return n
+def calc_fib(n):
+    array = [None] * (n + 1)
+    if n == 0:
+        return 0
+    array[0] = 0
+    array[1] = 1
+    for i in range(2, n + 1):
+        array[i] = array[i - 1] + array[i - 2]
+    return array[n]
 
-    previous = 0
-    current  = 1
-
-    for _ in range(n - 1):
-        previous, current = current, previous + current
-
-    return current % m
+def get_fibonacci_huge(n, m):
+    length_cycle = m**2 - 1
+    remainder = n % length_cycle
+    return calc_fib(remainder) % m
 
 if __name__ == '__main__':
-    input = sys.stdin.read();
+    input = input()
+    # input = sys.stdin.read()
     n, m = map(int, input.split())
-    print(get_fibonacci_huge_naive(n, m))
+    print(get_fibonacci_huge(n, m))
