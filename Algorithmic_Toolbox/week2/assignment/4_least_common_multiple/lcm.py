aefb5b5e01c0332e0467f80e3b07cde8539d87b9
Yuna Luzi <junaluzi@gmail.com>
2019-04-14 10:25:17 -0400

    week 2 assignment part 4 lcm done

--- Algorithmic_Toolbox/week2/assignment/4_least_common_multiple/lcm.py
+++ Algorithmic_Toolbox/week2/assignment/4_least_common_multiple/lcm.py
@@ -1,15 +1,18 @@
 # Uses python3
 import sys
 
-def lcm_naive(a, b):
-    for l in range(1, a*b + 1):
-        if l % a == 0 and l % b == 0:
-            return l
+def gcd(a, b):
+	if b == 0:
+		return a
+	a_ = a % b
+	return gcd(b, a_)
 
-    return a*b
+def lcm_naive(a, b):
+	return a*b // gcd(a, b)
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    a, b = map(int, input.split())
-    print(lcm_naive(a, b))
+	# input = input()
+	input = sys.stdin.read()
+	a, b = map(int, input.split())
+	print(lcm_naive(a, b))
 
