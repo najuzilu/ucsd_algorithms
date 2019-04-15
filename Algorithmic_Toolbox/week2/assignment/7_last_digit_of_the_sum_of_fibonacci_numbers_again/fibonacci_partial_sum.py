c2c520d76803565b180185e55829eef1f44b3fe6
Yuna Luzi <junaluzi@gmail.com>
2019-04-15 10:03:13 -0400

    algo toolbox week2 assignment part 7 done

--- Algorithmic_Toolbox/week2/assignment/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
+++ Algorithmic_Toolbox/week2/assignment/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
@@ -1,22 +1,49 @@
 # Uses python3
 import sys
 
-def fibonacci_partial_sum_naive(from_, to):
-    sum = 0
+def calc_fib(n1, n2):
+	answer = 0
+	array = []
+	if n2 == 0:
+		return 0
+	array.append(0)
+	array.append(1)
+	if n1 <= 1:
+		answer += 1
+	for i in range(2, n2 + 1):
+		array.append((array[i - 1] + array[i - 2]))
+		if i >= n1:
+			answer += array[i]
+	return answer
 
-    current = 0
-    next  = 1
+def get_pisano_length(n):
+	array = []
+	if n < 1:
+		return n
+	array.append(0)
+	array.append(1)
+	for i in range(2, n*n + 1):
+		next_ = (array[i - 1] + array[i - 2]) % n
+		if (array[i-1] == 0) and (next_ == 1):
+			array.pop()
+			break
+		else:
+			array.append(next_)
+	return array
 
-    for i in range(to + 1):
-        if i >= from_:
-            sum += current
+def fibonacci_sum(n1, n2):
+	m = 10
+	length_cycle = len(get_pisano_length(m))
 
-        current, next = next, current + next
+	remainder2 = n2 % length_cycle
+	remainder1 = n1 % length_cycle
+	return calc_fib(remainder1, remainder2) % m
 
-    return sum % 10
-
+def fibonacci_partial_sum(from_, to):
+	return fibonacci_sum(from_, to)
 
 if __name__ == '__main__':
-    input = sys.stdin.read();
-    from_, to = map(int, input.split())
-    print(fibonacci_partial_sum_naive(from_, to))
+	input = sys.stdin.read()
+	# input = input()
+	from_, to = map(int, input.split())
+	print(fibonacci_partial_sum(from_, to))
