d685b999388efd64c9302b6cdf438a548afc28cb
Yuna Luzi <junaluzi@gmail.com>
2019-04-15 10:30:00 -0400

    algo toolbox week 2 assignment part 8 done

--- Algorithmic_Toolbox/week2/assignment/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.py
+++ Algorithmic_Toolbox/week2/assignment/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.py
@@ -1,20 +1,41 @@
 # Uses python3
 from sys import stdin
 
-def fibonacci_sum_squares_naive(n):
-    if n <= 1:
-        return n
+def calc_fib(n):
+	array = []
+	if n == 0:
+		return 0
+	array.append(0)
+	array.append(1)
+	for i in range(2, n + 2):
+		array.append((array[i - 1] + array[i - 2]))
+	return array[n] * array[n + 1]
 
-    previous = 0
-    current  = 1
-    sum      = 1
+def get_pisano_length(n):
+	array = []
+	if n < 1:
+		return n
+	array.append(0)
+	array.append(1)
+	for i in range(2, n*n + 1):
+		next_ = (array[i - 1] + array[i - 2]) % n
+		if (array[i-1] == 0) and (next_ == 1):
+			array.pop()
+			break
+		else:
+			array.append(next_)
+	return array
 
-    for _ in range(n - 1):
-        previous, current = current, previous + current
-        sum += current * current
+def fibonacci_sum(n):
+	m = 10
+	length_cycle = len(get_pisano_length(m))
+	remainder = n % length_cycle
+	return calc_fib(remainder) % m
 
-    return sum % 10
+def fibonacci_sum_squares(n):
+	return fibonacci_sum(n)
 
 if __name__ == '__main__':
-    n = int(stdin.read())
-    print(fibonacci_sum_squares_naive(n))
+	n = int(stdin.read())
+	# n = int(input())
+	print(fibonacci_sum_squares(n))
