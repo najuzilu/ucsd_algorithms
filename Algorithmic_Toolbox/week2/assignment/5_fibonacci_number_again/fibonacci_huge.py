821ee102e7de80fe43664be768b5c72b3d2e6239
Yuna Luzi <junaluzi@gmail.com>
2019-04-15 09:07:42 -0400

    algo toolbox week2 assignment part5 corrected

--- Algorithmic_Toolbox/week2/assignment/5_fibonacci_number_again/fibonacci_huge.py
+++ Algorithmic_Toolbox/week2/assignment/5_fibonacci_number_again/fibonacci_huge.py
@@ -2,22 +2,37 @@
 import sys
 
 def calc_fib(n):
-    array = [None] * (n + 1)
-    if n == 0:
-        return 0
-    array[0] = 0
-    array[1] = 1
-    for i in range(2, n + 1):
-        array[i] = array[i - 1] + array[i - 2]
-    return array[n]
+	array = [None] * (n + 1)
+	if n == 0:
+		return 0
+	array[0] = 0
+	array[1] = 1
+	for i in range(2, n + 1):
+		array[i] = array[i - 1] + array[i - 2]
+	return array[n]
+
+def get_pisano_length(n):
+	array = []
+	if n < 1:
+		return n
+	array.append(0)
+	array.append(1)
+	for i in range(2, n*n + 1):
+		next_ = (array[i - 1] + array[i - 2]) % n
+		if (array[i-1] == 0) and (next_ == 1):
+			array.pop()
+			break
+		else:
+			array.append(next_)
+	return array
 
 def get_fibonacci_huge(n, m):
-    length_cycle = m**2 - 1
-    remainder = n % length_cycle
-    return calc_fib(remainder) % m
+	length_cycle = len(get_pisano_length(m))
+	remainder = n % length_cycle
+	return calc_fib(remainder) % m
 
 if __name__ == '__main__':
-    input = input()
-    # input = sys.stdin.read()
-    n, m = map(int, input.split())
-    print(get_fibonacci_huge(n, m))
+	# input = input()
+	input = sys.stdin.read()
+	n, m = map(int, input.split())
+	print(get_fibonacci_huge(n, m))
