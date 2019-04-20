7fc5fcc0929d78c0f155491b7b379a50ffca75ea
Yuna Luzi <junaluzi@gmail.com>
2019-04-20 18:24:13 -0400

    data stru: is_bst_hard week 6 done

--- Data_Structures/week6/assignment/is_bst_hard/is_bst_hard.py
+++ Data_Structures/week6/assignment/is_bst_hard/is_bst_hard.py
@@ -3,51 +3,51 @@
 import sys, threading
 
 sys.setrecursionlimit(10**7) # max depth of recursion
-threading.stack_size(2**25)  # new thread will get stack of such size
+threading.stack_size(2**30)  # new thread will get stack of such size
 
-'''
-(2)is_bst_hard: use recursive method to determine each node whether it fits the requirement of a binary search tree. The trick is to write a function that traverses down the tree keeping track of the narrowing min and max allowed values as it goes, looking at each node only once.
-function isBST(node, min, max){
-    ...
-    #If the node violate the max/min contraint, it's not a BST
-    if (node <= min or node > max){
-        return False
-    }
-    ...
-    #For the left child, its maximum is root node
-    #For the right child, its minimum is root node
-    return isBST(leftnode, min, node) && isBST(rightnode, node, max)
-    ...
-}
-'''
+def IsBinarySearchTree():
+	global tree
 
-def IsBinarySearchTree(tree):
 	if len(tree) < 1:
 		return True
-	root = tree[0]
-	isBst(root, 0, 0)
+	answer = inOrder()
+	return answer
 
-def isBst(node, min_value, max_value):
-	print(node[0], node[1], node[2], min_value, max_value)
-	if (node[0] <= min_value) or (node[0] > max_value):
+def inOrder(i = 0):
+	global tree
+	global prev_value
+
+	node = tree[i]
+	if node[1] != -1:
+		if not inOrder(node[1]):
+			return False
+	# print(node, ' --  ', prev_value)
+	if node[0] < prev_value:
 		return False
-	return isBst(node[1], min_value, node[0]) and isBST(node[2], node[0], max_value)
+	elif node[0] == prev_value:
+		if node[1] != -1:
+			return False
+	else:
+		prev_value = node[0]
 
+	if node[2] != -1:
+		if not inOrder(node[2]):
+			return False
+	return True	
 
 def main():
+	global tree
 	nodes = int(sys.stdin.readline().strip())
 	tree = []
 	for i in range(nodes):
 		tree.append(list(map(int, sys.stdin.readline().strip().split())))
 	
-	IsBinarySearchTree(tree)
-	# if IsBinarySearchTree(tree):
-	# 	print("CORRECT")
-	# else:
-	# 	print("INCORRECT")
+	if IsBinarySearchTree():
+		print("CORRECT")
+	else:
+		print("INCORRECT")
 
 if __name__ == '__main__':
 	# python is_bst_hard.py < tests/3
-	min_value = -float("inf")
-	max_value = float("inf")
+	prev_value = -float("inf")
 	threading.Thread(target=main).start()
