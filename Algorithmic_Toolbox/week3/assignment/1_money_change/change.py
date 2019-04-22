0e8d403e852805b4deb62fb7550fc08eec1a4ff6
Yuna Luzi <junaluzi@gmail.com>
2019-04-22 08:00:48 -0400

    algo toolbox week 3 money change assignment done

--- Algorithmic_Toolbox/week3/assignment/1_money_change/change.py
+++ Algorithmic_Toolbox/week3/assignment/1_money_change/change.py
@@ -2,9 +2,15 @@
 import sys
 
 def get_change(m):
-    #write your code here
-    return m
+	remainder_10 = m // 10
+	m = m - (remainder_10 * 10)
+
+	remainder_5 = m // 5
+	m = m - (remainder_5 * 5)
+	
+	return remainder_10 + remainder_5 + m
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
-    print(get_change(m))
+	m = int(sys.stdin.read())
+	# m = int(input())
+	print(get_change(m))
