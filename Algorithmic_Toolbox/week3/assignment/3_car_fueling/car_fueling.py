9d9085565142f7750be961f01fcd8cdc32655501
Yuna Luzi <junaluzi@gmail.com>
2019-04-22 11:40:40 -0400

    algo toolbox week 3 assignment car fueling done

--- Algorithmic_Toolbox/week3/assignment/3_car_fueling/car_fueling.py
+++ Algorithmic_Toolbox/week3/assignment/3_car_fueling/car_fueling.py
@@ -1,11 +1,28 @@
 # python3
 import sys
 
+# python car_fueling.py < tests/1
 
 def compute_min_refills(distance, tank, stops):
-    # write your code here
-    return -1
+	stops = [0] + stops + [distance]
+
+	numRefills = 0
+	currentRefillIndex = 0
+
+	while currentRefillIndex <= len(stops)-2:
+		lastRefillIndex = currentRefillIndex
+
+		while (currentRefillIndex <=  len(stops)-2) and (stops[currentRefillIndex+1]-stops[lastRefillIndex] <= tank):
+			currentRefillIndex += 1
+
+		if currentRefillIndex == lastRefillIndex:
+			return -1
+
+		if currentRefillIndex <= len(stops)-2:
+			numRefills += 1
+	return numRefills
+
 
 if __name__ == '__main__':
-    d, m, _, *stops = map(int, sys.stdin.read().split())
-    print(compute_min_refills(d, m, stops))
+	d, m, _, *stops = map(int, sys.stdin.read().split())
+	print(compute_min_refills(d, m, stops))
