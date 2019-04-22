942435c73022071bdcea2dadc09a0baabe172179
Yuna Luzi <junaluzi@gmail.com>
2019-04-22 08:47:57 -0400

    algo toolbox week3 knapsack problem done

--- Algorithmic_Toolbox/week3/assignment/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ Algorithmic_Toolbox/week3/assignment/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -1,17 +1,34 @@
 # Uses python3
+
+# python fractional_knapsack.py < tests/1
+
 import sys
 
 def get_optimal_value(capacity, weights, values):
-    value = 0.
-    # write your code here
+	value = 0.
+	# calculate value per 1 weight
+	val_per_weight = [x/y for x, y in zip(values, weights)]
+	
+	while capacity != 0 and len(val_per_weight) != 0:
+		index_max = val_per_weight.index(max(val_per_weight))
 
-    return value
+		if weights[index_max] / capacity <= 1:
+			capacity = capacity - weights[index_max]
+			value += values[index_max]
+			# pop items and continue while loop
+			weights.pop(index_max)
+			values.pop(index_max)
+			val_per_weight.pop(index_max)
+		else:
+			value += (capacity / weights[index_max]) * values[index_max]
+			capacity -= capacity
+	return value
 
 
 if __name__ == "__main__":
-    data = list(map(int, sys.stdin.read().split()))
-    n, capacity = data[0:2]
-    values = data[2:(2 * n + 2):2]
-    weights = data[3:(2 * n + 2):2]
-    opt_value = get_optimal_value(capacity, weights, values)
-    print("{:.10f}".format(opt_value))
+	data = list(map(int, sys.stdin.read().split()))
+	n, capacity = data[0:2]
+	values = data[2:(2 * n + 2):2]
+	weights = data[3:(2 * n + 2):2]
+	opt_value = get_optimal_value(capacity, weights, values)
+	print("{:.10f}".format(opt_value))
