cdc2869beb9d7a21aaff7b743a1613afb7b1a622
Yuna Luzi <junaluzi@gmail.com>
2019-04-23 08:54:56 -0400

    algo toolbox week3 collecting signiture making progress but not there yet

--- Algorithmic_Toolbox/week3/assignment/5_collecting_signatures/covering_segments.py
+++ Algorithmic_Toolbox/week3/assignment/5_collecting_signatures/covering_segments.py
@@ -4,12 +4,31 @@ from collections import namedtuple
 
 Segment = namedtuple('Segment', 'start end')
 
+
 def optimal_points(segments):
     points = []
-    #write your code here
-    for s in segments:
-        points.append(s.start)
-        points.append(s.end)
+
+    all_unions = []
+
+    segments = sorted(segments, key=lambda x: x.end)
+
+    for i in range(1, len(segments)):
+        current_s = segments[i]
+        prev_s = segments[i-1]
+        current_range = list(range(current_s.start, current_s.end+1))
+        prev_range = list(range(prev_s.start, prev_s.end+1))
+        union = list(set(prev_range) & set(current_range))
+        union.sort()
+        all_unions.append(union)
+
+    for j in range(1, len(all_unions)):
+        print(all_unions[j-1], all_unions[j])
+        print(list(set(all_unions[j-1]) & set(all_unions[j])))
+        # if list(set(all_unions[j-1]) & set(all_unions[j])) == []:
+        #     points.append(all_unions[j-1][-1])
+        # else:
+        #     points.append(all_unions[j][-1])
+        
     return points
 
 if __name__ == '__main__':
@@ -20,3 +39,4 @@ if __name__ == '__main__':
     print(len(points))
     for p in points:
         print(p, end=' ')
+    print('\n')
