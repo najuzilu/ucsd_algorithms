663a7e70a4cddeb5069ffb46d715480e8ce1037d
Yuna Luzi <junaluzi@gmail.com>
2019-04-23 19:40:58 -0400

    algo toolbox week3 max number of prizes done

--- Algorithmic_Toolbox/week3/assignment/6_maximum_number_of_prizes/different_summands.py
+++ Algorithmic_Toolbox/week3/assignment/6_maximum_number_of_prizes/different_summands.py
@@ -1,15 +1,25 @@
 # Uses python3
 import sys
+import math
 
 def optimal_summands(n):
-    summands = []
-    #write your code here
-    return summands
+	summands = []
+
+	k = int(-1/2 + math.sqrt(1/4 + 2 * n))
+
+	if k == 1:
+		summands.append(n)
+		return summands
+	for each in range(1, k):
+		summands.append(each)
+	last_item = n - sum(summands)
+	summands.append(last_item)
+	return summands
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n = int(input)
-    summands = optimal_summands(n)
-    print(len(summands))
-    for x in summands:
-        print(x, end=' ')
+	input = sys.stdin.read()
+	n = int(input)
+	summands = optimal_summands(n)
+	print(len(summands))
+	for x in summands:
+		print(x, end=' ')
