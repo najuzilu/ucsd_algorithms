f0fec3e9e089944da654938d21d7d000043a962f
Yuna Luzi <junaluzi@gmail.com>
2019-04-23 19:41:19 -0400

    algo toolbox week3 covering segments fails #12/15

--- Algorithmic_Toolbox/week3/assignment/5_collecting_signatures/covering_segments.py
+++ Algorithmic_Toolbox/week3/assignment/5_collecting_signatures/covering_segments.py
@@ -4,31 +4,38 @@ from collections import namedtuple
 
 Segment = namedtuple('Segment', 'start end')
 
+def in_list(list_, list_points):
+    flag = False
+    for each in list_:
+        if each in list_points:
+            flag = True
+    return flag
+
 
 def optimal_points(segments):
     points = []
-
-    all_unions = []
-
     segments = sorted(segments, key=lambda x: x.end)
 
-    for i in range(1, len(segments)):
+    segments.append(segments[-1])
+
+    for i in range(1, len(segments)): #len(segments)
         current_s = segments[i]
         prev_s = segments[i-1]
         current_range = list(range(current_s.start, current_s.end+1))
         prev_range = list(range(prev_s.start, prev_s.end+1))
-        union = list(set(prev_range) & set(current_range))
-        union.sort()
-        all_unions.append(union)
 
-    for j in range(1, len(all_unions)):
-        print(all_unions[j-1], all_unions[j])
-        print(list(set(all_unions[j-1]) & set(all_unions[j])))
-        # if list(set(all_unions[j-1]) & set(all_unions[j])) == []:
-        #     points.append(all_unions[j-1][-1])
-        # else:
-        #     points.append(all_unions[j][-1])
-        
+        if in_list(prev_range, points):
+            continue
+        union = list(set(prev_range) & set(current_range))
+        if union != []:
+            union.sort()
+            points.append(union[-1])
+        else:
+            if prev_range[-1] not in points:
+                points.append(prev_range[-1])
+            if current_range[-1] not in points:
+                points.append(current_range[-1])
+               
     return points
 
 if __name__ == '__main__':
@@ -39,4 +46,3 @@ if __name__ == '__main__':
     print(len(points))
     for p in points:
         print(p, end=' ')
-    print('\n')
