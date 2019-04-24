bf1431f92c6fd68945c9a958113f24e253dace37
Yuna Luzi <junaluzi@gmail.com>
2019-04-23 20:42:08 -0400

    algo toolbox week3 max salary done

--- Algorithmic_Toolbox/week3/assignment/7_maximum_salary/largest_number.py
+++ Algorithmic_Toolbox/week3/assignment/7_maximum_salary/largest_number.py
@@ -3,15 +3,23 @@
 import sys
 
 def largest_number(a):
-    #write your code here
-    res = ""
-    for x in a:
-        res += x
-    return res
+	res = ""
+	# a.sort(key = lambda item: item[0], reverse = True)
+	
+	while a != []:
+		maxDigit = '0'
+		for digit in a:
+			if digit[0] > maxDigit[0]:
+				maxDigit = digit
+			elif digit[0] == maxDigit[0]:
+				if len(digit) < len(maxDigit):
+					maxDigit = digit
+		res += maxDigit
+		a.remove(maxDigit)
+	return res
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = input.split()
-    a = data[1:]
-    print(largest_number(a))
-    
+	input = sys.stdin.read()
+	data = input.split()
+	a = data[1:]
+	print(largest_number(a))
