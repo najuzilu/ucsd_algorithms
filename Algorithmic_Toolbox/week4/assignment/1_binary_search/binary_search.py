ea42d3f245908fe8f07806eaee2ce75c5761a7f8
Yuna Luzi <junaluzi@gmail.com>
2019-05-27 12:11:25 -0400

    week4 1_bin_search done

--- Algorithmic_Toolbox/week4/assignment/1_binary_search/binary_search.py
+++ Algorithmic_Toolbox/week4/assignment/1_binary_search/binary_search.py
@@ -1,22 +1,35 @@
 # Uses python3
 import sys
+# sys.setrecursionlimit(1500)
 
-def binary_search(a, x):
-    left, right = 0, len(a)
-    # write your code here
+def binary_search(a, x, left_idx, right_idx):
+	if right_idx <= left_idx:
+		return -1
+	mid_idx = int(left_idx + (right_idx - left_idx)/2)
+	if x == a[mid_idx]:
+		return mid_idx
+	elif x < a[mid_idx]:
+		if right_idx - left_idx == 1: return -1
+		right_idx = mid_idx
+		return binary_search(a, x, left_idx, right_idx)
+	elif x > a[mid_idx]:
+		if right_idx - left_idx == 1: return -1
+		left_idx = mid_idx
+		return binary_search(a, x, left_idx, right_idx)
 
 def linear_search(a, x):
-    for i in range(len(a)):
-        if a[i] == x:
-            return i
-    return -1
+	for i in range(len(a)):
+		if a[i] == x:
+			return i
+	return -1
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n = data[0]
-    m = data[n + 1]
-    a = data[1 : n + 1]
-    for x in data[n + 2:]:
-        # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+	input = sys.stdin.read()
+	data = list(map(int, input.split()))
+	n = data[0]
+	m = data[n + 1]
+	a = data[1 : n + 1]
+	left_idx, right_idx = 0, len(a)
+	for x in data[n + 2:]:
+		print(binary_search(a, x, left_idx, right_idx), end = ' ')
+	# 	print(linear_search(a, x), end = ' ') # linear_search implementation
