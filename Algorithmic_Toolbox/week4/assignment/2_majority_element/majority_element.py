70d18643503f4bfe066d95af0d3a826064107d55
Yuna Luzi <junaluzi@gmail.com>
2019-05-27 15:22:30 -0400

    algo toolbox week4 2_maj_el find left_and right elements

--- Algorithmic_Toolbox/week4/assignment/2_majority_element/majority_element.py
+++ Algorithmic_Toolbox/week4/assignment/2_majority_element/majority_element.py
@@ -2,17 +2,22 @@
 import sys
 
 def get_majority_element(a, left, right):
-    if left == right:
-        return -1
-    if left + 1 == right:
-        return a[left]
-    #write your code here
-    return -1
-
+	if left == right:
+		return -1
+	if left + 1 == right:
+		return a[left]
+	mid_point = int((right - left)/2 + left)
+	# Get left and right elements
+	left_el = get_majority_element(a, left, mid_point)
+	right_el = get_majority_element(a, mid_point + 1, right)
+	print(left_el, right_el)
+	return
+	
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *a = list(map(int, input.split()))
-    if get_majority_element(a, 0, n) != -1:
-        print(1)
-    else:
-        print(0)
+	input = sys.stdin.read()
+	n, *a = list(map(int, input.split()))
+	print(get_majority_element(a, 0, n))
+	# if get_majority_element(a, 0, n) != -1:
+	# 	print(1)
+	# else:
+	# 	print(0)
