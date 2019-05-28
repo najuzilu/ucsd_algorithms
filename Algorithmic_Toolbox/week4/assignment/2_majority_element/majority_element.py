0eeec5135c607892ef0d5757016ec24a84c7b816
Yuna Luzi <junaluzi@gmail.com>
2019-05-28 16:01:44 -0400

    algo toolbox week4 2_maj_el failed test case #6

--- Algorithmic_Toolbox/week4/assignment/2_majority_element/majority_element.py
+++ Algorithmic_Toolbox/week4/assignment/2_majority_element/majority_element.py
@@ -1,6 +1,15 @@
 # Uses python3
 import sys
 
+def count_if_majority(a, left, right, element):
+	count = 0
+	for i in range(right - left):
+		if a[left + i] == element:
+			count += 1
+	if count / (right- left) > 0.5:
+		return count 
+	return -1
+
 def get_majority_element(a, left, right):
 	if left == right:
 		return -1
@@ -8,16 +17,40 @@ def get_majority_element(a, left, right):
 		return a[left]
 	mid_point = int((right - left)/2 + left)
 	# Get left and right elements
-	left_el = get_majority_element(a, left, mid_point)
-	right_el = get_majority_element(a, mid_point + 1, right)
-	print(left_el, right_el)
-	return
+	if len(a[left:right]) == 2:
+		left_el = get_majority_element(a, left, mid_point)
+		right_el = get_majority_element(a, mid_point + 1, right)
+	else:
+		left_el = get_majority_element(a, left, mid_point)
+		right_el = get_majority_element(a, mid_point, right)
+	if left_el == -1 and right_el == -1:
+		# if both are -1
+		return -1
+	elif left_el != -1 and right_el == -1:
+		# if left_el
+		if count_if_majority(a, left, right, left_el) > 1:
+			return left_el
+		else:
+			return -1
+	elif left_el == -1 and right_el != -1:
+		# if right_el
+		if count_if_majority(a, left, right, right_el) > 1:
+			return right_el
+		else:
+			return -1
+	else:
+		# if both, count both and determine
+		left_count = count_if_majority(a, left, right, left_el)
+		right_count = count_if_majority(a, left, right, right_el)
+		if left_count >= right_count:
+			return left_el
+		else:
+			return right_el
 	
 if __name__ == '__main__':
 	input = sys.stdin.read()
 	n, *a = list(map(int, input.split()))
-	print(get_majority_element(a, 0, n))
-	# if get_majority_element(a, 0, n) != -1:
-	# 	print(1)
-	# else:
-	# 	print(0)
+	if get_majority_element(a, 0, n) != -1:
+		print(1)
+	else:
+		print(0)
