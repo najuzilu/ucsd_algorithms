54979dc6ebbd0bdbbee5bc453e767613fe539895
Yuna Luzi <junaluzi@gmail.com>
2019-05-29 14:58:36 -0400

    algo toolbox week4 2_maj_elm naive implementation done; divide and conquer alsmot done

--- Algorithmic_Toolbox/week4/assignment/2_majority_element/majority_element.py
+++ Algorithmic_Toolbox/week4/assignment/2_majority_element/majority_element.py
@@ -7,22 +7,22 @@ def count_if_majority(a, left, right, element):
 		if a[left + i] == element:
 			count += 1
 	if count / (right- left) > 0.5:
-		return count 
+		return count
 	return -1
 
-def get_majority_element(a, left, right):
+def get_majority_element_test(a, left, right):
+	'''
+		This approach is still in testing:
+		>> test case 9 should be 1 but is instead 0
+	'''
 	if left == right:
 		return -1
 	if left + 1 == right:
 		return a[left]
 	mid_point = int((right - left)/2 + left)
 	# Get left and right elements
-	if len(a[left:right]) == 2:
-		left_el = get_majority_element(a, left, mid_point)
-		right_el = get_majority_element(a, mid_point + 1, right)
-	else:
-		left_el = get_majority_element(a, left, mid_point)
-		right_el = get_majority_element(a, mid_point, right)
+	left_el = get_majority_element(a, left, mid_point)
+	right_el = get_majority_element(a, mid_point + 1, right)
 	if left_el == -1 and right_el == -1:
 		# if both are -1
 		return -1
@@ -46,7 +46,24 @@ def get_majority_element(a, left, right):
 			return left_el
 		else:
 			return right_el
-	
+
+def get_majority_element(a, left, right):
+	'''
+		This approach does not use divide and conquer
+		Naive implementation
+	'''
+	dict_row = {}
+	for each in a:
+		if each not in dict_row.keys():
+			dict_row[each] = 1
+		else:
+			dict_row[each] += 1
+	key_max = max(dict_row.keys(), key=(lambda k: dict_row[k]))
+	if dict_row[key_max] / len(a) > 0.5:
+		return dict_row[key_max]
+	else:
+		return -1
+
 if __name__ == '__main__':
 	input = sys.stdin.read()
 	n, *a = list(map(int, input.split()))
