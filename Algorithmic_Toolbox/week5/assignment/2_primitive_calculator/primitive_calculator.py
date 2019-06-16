934e171ee0977e58111ae35936772a24ae3e3692
Yuna Luzi <junaluzi@gmail.com>
2019-06-16 10:40:13 -0400

    algo toolbox week5 2_primitive_calculator done

--- Algorithmic_Toolbox/week5/assignment/2_primitive_calculator/primitive_calculator.py
+++ Algorithmic_Toolbox/week5/assignment/2_primitive_calculator/primitive_calculator.py
@@ -15,36 +15,42 @@ def optimal_sequence(n):
 	return reversed(sequence)
 
 def get_change(m):
+	n = m
 	denominations = [3, 2, 1]
 	min_num = [None] * (m + 1)
 	min_num[0], min_num[1] = 0, 0
-
 	results = []
-	results.append(1)
-
 	for i in range(2, m + 1):
 		min_num[i] = math.inf 
 		for j in range(len(denominations)):
 			if i >= denominations[j]:
-				if denominations[j] == 3:
+				if denominations[j] == 3 or denominations[j] == 2:
 					if i % denominations[j] == 0:
-						num_coin = min_num[i // 3] + 1
-					else:
-						num_coin = math.inf
-				elif denominations[j] == 2:
-					if i % denominations[j] == 0:
-						num_coin = min_num[i // 2] + 1
+						num_coin = min_num[i // denominations[j]] + 1
 					else:
 						num_coin = math.inf
 				else:
 					num_coin = min_num[i - 1] + 1
 				if num_coin < min_num[i]:
 					min_num[i] = num_coin
-					
-	for i in range(1, len(min_num) - 1):
-		if (min_num[i + 1] == min_num[i] + 1) or (min_num[i + 1] == 2 * min_num[i]) or (min_num[i + 1] == 3 * min_num[i]):
-			results.append(i)
-	return min_num[m], results
+
+	while n > 0:
+		min_value = math.inf
+		next_value = n
+		if min_num[n - 1] < min_value:
+			min_value = min_num[n - 1]
+			next_value = n - 1
+		if n % 3 == 0:
+			if min_num[n//3] < min_value:
+				min_value = min_num[n//3]
+				next_value = n // 3
+		if n % 2 == 0:
+			if min_num[n//2] <= min_value:
+				min_value = min_num[n//2]
+				next_value = n // 2
+		results.append(n)
+		n = next_value
+	return min_num[m], reversed(results)
 
 input = sys.stdin.read()
 n = int(input)
