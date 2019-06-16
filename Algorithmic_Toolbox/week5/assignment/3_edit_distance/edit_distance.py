e7e8f17e7be861bd86f957e5be53a038a6915ed0
Yuna Luzi <junaluzi@gmail.com>
2019-06-16 15:39:48 -0400

    algo toolbox week5 3_edit_distance done

--- Algorithmic_Toolbox/week5/assignment/3_edit_distance/edit_distance.py
+++ Algorithmic_Toolbox/week5/assignment/3_edit_distance/edit_distance.py
@@ -1,7 +1,26 @@
 # Uses python3
-def edit_distance(s, t):
-    #write your code here
-    return 0
+
+def edit_distance(s, t): # i, j
+	insertion, deletion, match, mismatch, min_val = 0, 0, 0, 0, 0
+
+	matrix = [[] * (len(t) + 1)] * (len(s) + 1)
+	matrix[0] = list(range(len(t) + 1)) # D(0, j) <- j for all j
+	for i in range(1, len(s) + 1): # D(i, 0) <- i for all i
+		matrix[i] = [i]
+
+	for j in range(1, len(t) + 1):
+		for i in range(1, len(s) + 1):
+			new_array = matrix[i].copy()
+			insertion = matrix[i][j - 1] + 1
+			deletion = matrix[i - 1][j] + 1
+			match = matrix[i - 1][j - 1]
+			mismatch = matrix[i - 1][j - 1] + 1
+			if s[i - 1] == t[j - 1]:
+				min_val = min(insertion, deletion, match)
+			else:
+				min_val = min(insertion, deletion, mismatch)
+			matrix[i].extend([min_val])
+	return matrix[-1][-1]
 
 if __name__ == "__main__":
-    print(edit_distance(input(), input()))
+	print(edit_distance(input(), input()))
