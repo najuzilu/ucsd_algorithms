dc9145d7e996c58a990f455ebe51876db2549842
Yuna Luzi <junaluzi@gmail.com>
2019-06-15 20:42:34 -0400

    algo toolbox week5 1_money_change_again done

--- Algorithmic_Toolbox/week5/assignment/1_money_change_again/change_dp.py
+++ Algorithmic_Toolbox/week5/assignment/1_money_change_again/change_dp.py
@@ -1,10 +1,21 @@
 # Uses python3
 import sys
+import math
 
 def get_change(m):
-    #write your code here
-    return m // 4
+	denominations = [1, 3, 4]
+	min_num_coins = [None] * (m + 1)
+	min_num_coins[0] = 0
+
+	for i in range(1, m + 1):
+		min_num_coins[i] = math.inf 
+		for j in range(len(denominations)):
+			if i >= denominations[j]:
+				num_coin = min_num_coins[i - denominations[j]] + 1
+				if num_coin < min_num_coins[i]:
+					min_num_coins[i] = num_coin
+	return min_num_coins[m]
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
-    print(get_change(m))
+	m = int(sys.stdin.read())
+	print(get_change(m))
