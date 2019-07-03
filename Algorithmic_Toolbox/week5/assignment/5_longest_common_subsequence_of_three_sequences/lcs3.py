0c3e58421e3e93fe251084095532278b997918c9
Yuna Luzi <junaluzi@gmail.com>
2019-07-03 16:19:55 -0400

    algo toolbox week5 assign 5_longest_common_Sequence_of_three.. fixed main matrix before calculating values

--- Algorithmic_Toolbox/week5/assignment/5_longest_common_subsequence_of_three_sequences/lcs3.py
+++ Algorithmic_Toolbox/week5/assignment/5_longest_common_subsequence_of_three_sequences/lcs3.py
@@ -4,64 +4,35 @@ import sys
 
 def edit_distance(a, b, c):
 	insert, deletion, match, mismatch, min_val = 0, 0, 0, 0, 0
-	matrix = [ [ [math.inf] * (len(c) + 1)] * (len(b) + 1)] * (len(a) + 1)
+	matrix = [[[math.inf for _ in range(len(c) + 1)] for _ in range(len(b) + 1)] for _ in range(len(a) + 1)]
 
-	# for m in range(len(a) + 1):
-		# matrix[m] = [[m]]
-	# for n in range(1, len(b) + 1):
-	# 	matrix[0].append([n])
-	# matrix[0][0] = list(range(len(c) + 1))
+	for m in range(len(a) + 1):
+		matrix[m][0][0] = m
+	for n in range(1, len(b) + 1):
+		matrix[0][n][0] = n
+	matrix[0][0] = list(range(len(c) + 1))
 
-
-	# matrix[0][0] = list(range(len(c) + 1))
-
-	
-	
-	print('\n',matrix)
+	for ech in matrix:
+		print(ech)
 
 	# for k in range(1, len(c) + 1):
 	# 	for j in range(1, len(b) + 1):
 	# 		for i in range(1, len(a) + 1):
-	# 			insertion = matrix[i - 1][j - 1][k] + 1
-	# 			deletion = matrix[i][j - 1][k - 1] + 1
-	# 			ins_del = matrix[i - 1][j][k - 1] + 1
+	# 			ins1 = matrix[i - 1][j][k] + 1
+	# 			ins2 = matrix[i][j - 1][k] + 1
+	# 			ins3 = matrix[i][j][k - 1] + 1
 	# 			match = matrix[i - 1][j - 1][k - 1]
 	# 			mismatch = matrix[i - 1][j - 1][k - 1] + 2
-	# 			if a[i] == b[j] == c[k]:
-	# 				min_val = min(insertion, deletion, ins_del, match)
+	# 			if a[i - 1] == b[j - 1] and b[j - 1] == c[k - 1]:
+	# 				min_val = min(ins1, ins2, ins3, match)
 	# 			else:
-	# 				min_val = min(insertion, deletion, ins_del, mismatch)
-	# 			print(matrix[i], min_val, i, j, k)
-	# 			break
-	# 		break
-	# 	break
-				
-				
+	# 				min_val = min(ins1, ins2, ins3, mismatch)
+	# 			matrix[i][j][k] = min_val
 
-
-
-
-
-	# edit_distance_matrix[0] = list(range(len(b) + 1))
-	# for m in range(1, len(a) + 1):
-	# 	edit_distance_matrix[m] = [m]
-
-	# for j in range(1, len(b) + 1):
-	# 	for i in range(1, len(a) + 1):
-	# 		new_array = edit_distance_matrix[i].copy()
-	# 		insertion = edit_distance_matrix[i][j - 1] + 1
-	# 		deletion = edit_distance_matrix[i - 1][j] + 1
-	# 		match = edit_distance_matrix[i - 1][j - 1]
-	# 		mismatch = edit_distance_matrix[i - 1][j - 1] + 2
-	# 		if a[i - 1] == b[j - 1]:
-	# 			min_val = min(insertion, deletion, match)
-	# 		else:
-	# 			min_val = min(insertion, deletion, mismatch)
-	# 		edit_distance_matrix[i].extend([min_val])
-	return 
+	return matrix[-1][-1][-1]
 
 def lcs3(a, b, c):
-	edit_distance(a, b, c)
+	print('answer=', edit_distance(a, b, c))
 	return min(len(a), len(b), len(c))
 
 if __name__ == '__main__':
