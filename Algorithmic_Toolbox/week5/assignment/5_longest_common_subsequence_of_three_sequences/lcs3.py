949d33f2e85f950f2773ca6784a78f2a3a127f10
Yuna Luzi <junaluzi@gmail.com>
2019-07-04 12:18:46 -0400

    algo toolbox week5 assign 5_longest_common_Sequence_of_three.. DONE

--- Algorithmic_Toolbox/week5/assignment/5_longest_common_subsequence_of_three_sequences/lcs3.py
+++ Algorithmic_Toolbox/week5/assignment/5_longest_common_subsequence_of_three_sequences/lcs3.py
@@ -3,37 +3,19 @@ import math
 import sys
 
 def edit_distance(a, b, c):
-	insert, deletion, match, mismatch, min_val = 0, 0, 0, 0, 0
-	matrix = [[[math.inf for _ in range(len(c) + 1)] for _ in range(len(b) + 1)] for _ in range(len(a) + 1)]
+	matrix = [[[0 for _ in range(len(c) + 1)] for _ in range(len(b) + 1)] for _ in range(len(a) + 1)]
 
-	for m in range(len(a) + 1):
-		matrix[m][0][0] = m
-	for n in range(1, len(b) + 1):
-		matrix[0][n][0] = n
-	matrix[0][0] = list(range(len(c) + 1))
-
-	for ech in matrix:
-		print(ech)
-
-	# for k in range(1, len(c) + 1):
-	# 	for j in range(1, len(b) + 1):
-	# 		for i in range(1, len(a) + 1):
-	# 			ins1 = matrix[i - 1][j][k] + 1
-	# 			ins2 = matrix[i][j - 1][k] + 1
-	# 			ins3 = matrix[i][j][k - 1] + 1
-	# 			match = matrix[i - 1][j - 1][k - 1]
-	# 			mismatch = matrix[i - 1][j - 1][k - 1] + 2
-	# 			if a[i - 1] == b[j - 1] and b[j - 1] == c[k - 1]:
-	# 				min_val = min(ins1, ins2, ins3, match)
-	# 			else:
-	# 				min_val = min(ins1, ins2, ins3, mismatch)
-	# 			matrix[i][j][k] = min_val
-
+	for k in range(1, len(c) + 1):
+		for j in range(1, len(b) + 1):
+			for i in range(1, len(a) + 1):
+				if a[i - 1] == b[j - 1] and b[j - 1] == c[k - 1]:
+					matrix[i][j][k] = matrix[i - 1][j - 1][k - 1] + 1
+				else:
+					matrix[i][j][k] = max(matrix[i - 1][j][k], matrix[i][j - 1][k], matrix[i][j][k - 1])
 	return matrix[-1][-1][-1]
 
 def lcs3(a, b, c):
-	print('answer=', edit_distance(a, b, c))
-	return min(len(a), len(b), len(c))
+	return edit_distance(a, b, c)
 
 if __name__ == '__main__':
 	input = sys.stdin.read()
