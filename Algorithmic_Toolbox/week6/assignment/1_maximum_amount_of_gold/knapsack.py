6f16b20fe1c10be6898dee0ca4604641be5f5cad
Yuna Luzi <junaluzi@gmail.com>
2019-07-04 13:19:17 -0400

    algo toolbox week6 1_max_amount_of_gold DONE

--- Algorithmic_Toolbox/week6/assignment/1_maximum_amount_of_gold/knapsack.py
+++ Algorithmic_Toolbox/week6/assignment/1_maximum_amount_of_gold/knapsack.py
@@ -1,15 +1,18 @@
 # Uses python3
 import sys
 
-def optimal_weight(W, w):
-    # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+def optimal_weight(W, n):
+	matrix = [[0 for _ in range(W + 1)] for _ in range(len(n) + 1)]
+	for i in range(1, len(n) + 1):
+		for j in range(1, W + 1):
+			matrix[i][j] = matrix[i - 1][j]
+			if n[i - 1] <= j:
+				val = matrix[i - 1][j - n[i - 1]] + n[i - 1]
+				if matrix[i][j] < val:
+					matrix[i][j] = val
+	return matrix[-1][-1]
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    W, n, *w = list(map(int, input.split()))
-    print(optimal_weight(W, w))
+	input = sys.stdin.read()
+	W, n, *w = list(map(int, input.split()))
+	print(optimal_weight(W, w))
