6453dbe16aaa2cf1d5809352ebd8d92f2b5d92a5
Yuna Luzi <junaluzi@gmail.com>
2019-07-07 13:06:30 -0400

    algo on string week1 assign trie DONE

--- Algorithms_on_String/week1/assignment/trie/trie.py
+++ Algorithms_on_String/week1/assignment/trie/trie.py
@@ -9,15 +9,43 @@ import sys
 # contains all the trie edges outgoing from the corresponding
 # node, and the keys are the letters on those edges, and the
 # values are the node IDs to which these edges lead.
-def build_trie(patterns):
-    tree = dict()
-    # write your code here
-    return tree
 
+def build_new_branch(tree, j, element, count):
+	for i in range(j, len(element)):
+		tree[count] = {element[i] : count + 1}
+		count += 1
+	return tree, count
+
+def build_trie(patterns):
+	tree = dict()
+	iter_ = 0
+	for i in range(len(patterns)):
+		element = patterns[i]
+		current_node = 0
+		if i == 0:
+			for j in range(len(element)):
+				string = element[j]
+				tree[iter_] = {string: iter_ + 1}
+				iter_ += 1
+		else:
+			for j in range(len(element)):
+				string = element[j]
+				if current_node not in tree.keys():
+					tree[current_node] = {}
+				if string not in tree[current_node]:
+					iter_ += 1
+					tree[current_node][string] = iter_
+					current_node = iter_
+					tree, iter_ = build_new_branch(tree, j + 1, element, current_node)
+					current_node += 1
+					break
+				else:
+					current_node = tree[current_node][string]
+	return tree
 
 if __name__ == '__main__':
-    patterns = sys.stdin.read().split()[1:]
-    tree = build_trie(patterns)
-    for node in tree:
-        for c in tree[node]:
-            print("{}->{}:{}".format(node, tree[node][c], c))
+	patterns = sys.stdin.read().split()[1:]
+	tree = build_trie(patterns)
+	for node in tree:
+		for c in tree[node]:
+			print("{}->{}:{}".format(node, tree[node][c], c))
