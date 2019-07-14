8d25f792b8331777a286ab83814338ec79b08965
Yuna Luzi <junaluzi@gmail.com>
2019-07-14 16:05:15 -0400

    algo on string week2 assignment bwt done

--- Algorithms_on_String/week2/assignment/bwt/bwt.py
+++ Algorithms_on_String/week2/assignment/bwt/bwt.py
@@ -2,8 +2,16 @@
 import sys
 
 def BWT(text):
-    return ""
+	array = []
+	result = ''
+	for i in range(len(text)):
+		text = text[-1] + text[:-1]
+		array.append(text)
+	sorted_array = sorted(array)
+	for each in sorted_array:
+		result += each[-1]
+	return result
 
 if __name__ == '__main__':
-    text = sys.stdin.readline().strip()
-    print(BWT(text))
+	text = sys.stdin.readline().strip()
+	print(BWT(text))
