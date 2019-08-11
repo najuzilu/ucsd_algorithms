566f94e0e4d534e4a39040c6dcc8f6245f5eb9d7
Yuna Luzi <junaluzi@gmail.com>
2019-08-11 19:13:33 -0400

    algo on string week1 assign suffix_tree fixed for test_samples # 1-4

--- Algorithms_on_String/week1/assignment/suffix_tree/suffix_tree.py
+++ Algorithms_on_String/week1/assignment/suffix_tree/suffix_tree.py
@@ -1,13 +1,7 @@
 # python3
 import sys
 
-def build_suffix_tree(text):
-	"""
-	Build a suffix tree of the string text and return a list
-	with all of the labels of its edges (the corresponding 
-	substrings of the text) in any order.
-	"""
-	print('Looping through text =', text)
+def suffix_tree(text):
 	tree = {} 
 	iter_ = 0 # to keep track # of nodes
 	
@@ -17,58 +11,66 @@ def build_suffix_tree(text):
 	iter_ += 1
 	length_cst = len(text) # length of text constant
 
-	for i in range(1, len(text)): # - 4
-		print('LOOPING THROUGH ', text[i:])
+	for i in range(1, len(text)):
 		cur = 0 # set current node to root node for each iter of text
 		count = 0 # count # of same letters
 		if text[i] in tree[cur].keys(): # if first letter in root
-			cur_next = tree[cur][text[i]] # find node after root
-			cur_next_info = inf_list[cur_next - 1] # subtract one because inf_list starts index 0
-			start_idx, length = cur_next_info[0], cur_next_info[1]
+			cur = tree[cur][text[i]] # find node after root
+			cur_info = inf_list[cur - 1] # subtract one because inf_list starts index 0
+			start_idx, length = cur_info[0], cur_info[1]
 			j = i
 			while text[j] == text[start_idx]:
-				print('i={}, j={}, start_idx={}, text[i]={}, text[j]={}, text[start_idx]={},length={}'.format(i,j,start_idx,text[i], text[j], text[start_idx], length))
 				count += 1
 				start_idx += 1
 				j += 1
-				if length - 1 == 0:
-					cur = tree[cur][text[i]] # find node after root
-					cur_next_info = inf_list[cur - 1] # subtract one because inf_list starts index 0
-					start_idx, length = cur_next_info[0], cur_next_info[1]
-					# j += 1
-				
-					print('update new node: ', cur)
-					print('>>i={}, j={}, start_idx={}, text[i]={}, text[j]={}, text[start_idx]={},length={}'.format(i,j,start_idx,text[i], text[j], text[start_idx], length))
+				length -= 1
+				if length == 0:
+					if text[j] in tree[cur].keys():
+						cur = tree[cur][text[j]] # find node after root
+						cur_info = inf_list[cur - 1] # subtract one because inf_list starts index 0
+						start_idx, length = cur_info[0], cur_info[1]
+						count -= 1
+			if cur in tree.keys():
+				# update tree with new node
+				tree[cur][text[j]] = iter_ + 1
+				inf_list[iter_] = [j, length_cst - j]
+				iter_ += 1
+			else:
+				# update inf_list with the new node with parent node information
+				inf_list[iter_] = [inf_list[cur - 1][0] + count, inf_list[cur - 1][1] - count]
+				# update inf_list parent node
+				inf_list[cur - 1][1] = count
+				# create the new node from the parent node info
+				tree[cur] = {text[start_idx] : iter_ + 1}
+				iter_ += 1
 
-			print('iter_={}, count={}, j={}'.format(iter_,count, j))
-			# update inf_list with the new node with parent node information
-			inf_list[iter_] = [inf_list[tree[cur][text[i]] - 1][0] + count, inf_list[tree[cur][text[i]] - 1][1] - count]
-			# update inf_list parent node
-			inf_list[tree[cur][text[i]] - 1][1] = count
-			# create the new node from the parent node info
-			tree[tree[cur][text[i]]] = {text[inf_list[tree[cur][text[i]] - 1][0] + count] : iter_ + 1}
-			iter_ += 1
-
-			# update inf_list with the new branch
-			inf_list[iter_] = [j, length_cst - j]
-			# create the new node from the parent node info
-			tree[tree[cur][text[i]]][text[j]] = iter_ + 1
-			iter_ += 1
-
+				# update inf_list with the new branch
+				inf_list[iter_] = [j, length_cst - j]
+				# create the new node from the parent node info
+				tree[cur][text[j]] = iter_ + 1
+				iter_ += 1
 		else: # if first letter not in root
 			tree[cur][text[i]] = iter_ + 1
 			inf_list[iter_] = [i, len(text) - i]
 			iter_ += 1
+	return tree, inf_list
 
-		print(tree)
-		print(inf_list)
-		print('\n')
-	print('\n')
-	print(tree)
-	print(inf_list)
+def build_suffix_tree(text):
+	"""
+	Build a suffix tree of the string text and return a list
+	with all of the labels of its edges (the corresponding 
+	substrings of the text) in any order.
+	"""
+	results = []
+	tree, suffix_tree_inf = suffix_tree(text)
 
-	return ''
-
+	for node in tree:
+		for c in tree[node]:
+			text_range = suffix_tree_inf[tree[node][c] - 1]
+			start_index = text_range[0]
+			text_len = text_range[1]
+			results.append(text[start_index: start_index + text_len])
+	return results
 
 if __name__ == '__main__':
 	text = sys.stdin.readline().strip()
