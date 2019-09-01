281c343a2f0f9cba68a03359732093f25a1587f8
najuzilu <junaluzi@gmail.com>
2019-08-31 22:01:48 -0400

    algo on graphs week1 hw minor clean-up

--- Algorithms_on_Graphs/week1/assignment/1_reachability/reachability.py
+++ Algorithms_on_Graphs/week1/assignment/1_reachability/reachability.py
@@ -3,19 +3,33 @@
 import sys
 
 def reach(adj, x, y):
-    #write your code here
-    return 0
+	#write your code here
+	print('{}, {}-{}'.format(adj, x, y))
+	for i in range(len(adj)):
+		try:
+			adj[i][1]
+			print('i ={}, adj[0]={}, adj[1]={}'.format(i, adj[i][0], adj[i][1]))
+			continue
+		except IndexError:
+			pass
+		try:
+			adj[i][0]
+			print('i ={}, adj[0]={}'.format(i, adj[i][0]))
+			continue
+		except IndexError:
+			pass
+	return 0
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n, m = data[0:2]
-    data = data[2:]
-    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
-    x, y = data[2 * m:]
-    adj = [[] for _ in range(n)]
-    x, y = x - 1, y - 1
-    for (a, b) in edges:
-        adj[a - 1].append(b - 1)
-        adj[b - 1].append(a - 1)
-    print(reach(adj, x, y))
+	input = sys.stdin.read()
+	data = list(map(int, input.split()))
+	n, m = data[0:2]
+	data = data[2:]
+	edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+	x, y = data[2 * m:]
+	adj = [[] for _ in range(n)]
+	x, y = x - 1, y - 1
+	for (a, b) in edges:
+		adj[a - 1].append(b - 1)
+		adj[b - 1].append(a - 1)
+	print(reach(adj, x, y))
