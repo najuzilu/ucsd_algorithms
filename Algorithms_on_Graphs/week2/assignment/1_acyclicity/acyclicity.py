cb45adc54aca203dde2971e38837c0f49b98bfef
najuzilu <junaluzi@gmail.com>
2019-09-26 12:05:15 +0200

    algo on graphs wk2 acyclicity assign done

--- Algorithms_on_Graphs/week2/assignment/1_acyclicity/acyclicity.py
+++ Algorithms_on_Graphs/week2/assignment/1_acyclicity/acyclicity.py
@@ -2,17 +2,32 @@
 
 import sys
 
+def explore(v, visited, i):
+	visited[v] = True
+	for node in adj[v]:
+		if visited[node] == False:
+			explore(node, visited, i)
+		if node == i:
+			global result
+			result = 1
 
 def acyclic(adj):
-    return 0
+	for i in range(len(adj)):
+		visited = [False for _ in range(len(adj))]
+		if visited[i] == False:
+			explore(i, visited, i)
+			if result == 1:
+				return 1
+	return 0
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n, m = data[0:2]
-    data = data[2:]
-    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
-    adj = [[] for _ in range(n)]
-    for (a, b) in edges:
-        adj[a - 1].append(b - 1)
-    print(acyclic(adj))
+	result = 0
+	input = sys.stdin.read()
+	data = list(map(int, input.split()))
+	n, m = data[0:2]
+	data = data[2:]
+	edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+	adj = [[] for _ in range(n)]
+	for (a, b) in edges:
+		adj[a - 1].append(b - 1)
+	print(acyclic(adj))
