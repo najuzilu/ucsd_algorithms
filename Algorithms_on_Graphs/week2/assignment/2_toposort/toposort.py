78f5cbdce430656c8a7d9818de20ed2f3d5166d8
najuzilu <junaluzi@gmail.com>
2019-10-10 17:11:57 +0200

    algo on graphs wk2 assign toposort progress

--- Algorithms_on_Graphs/week2/assignment/2_toposort/toposort.py
+++ Algorithms_on_Graphs/week2/assignment/2_toposort/toposort.py
@@ -2,27 +2,41 @@
 
 import sys
 
-def dfs(adj, used, order, x):
-    #write your code here
-    pass
+def explore(node, visited, previous):
+	visited[node] = True
 
+	for child in adj[node]:
+		if visited[child] == False:
+			explore(child, visited, previous)
 
+	ranked.insert(0, node)
+	if adj[node] == []:
+		if previous != node:
+			adj[previous].remove(node)
+
+def DFS(g):
+	visited = [False for _ in range(len(adj))]
+	for i in range(len(adj)):
+		if visited[i] == False:
+			explore(i, visited, i)
+	
 def toposort(adj):
-    used = [0] * len(adj)
-    order = []
-    #write your code here
-    return order
+	global ranked
+	ranked = []
+	DFS(adj)
+	return ranked
+
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n, m = data[0:2]
-    data = data[2:]
-    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
-    adj = [[] for _ in range(n)]
-    for (a, b) in edges:
-        adj[a - 1].append(b - 1)
-    order = toposort(adj)
-    for x in order:
-        print(x + 1, end=' ')
+	input = sys.stdin.read()
+	data = list(map(int, input.split()))
+	n, m = data[0:2]
+	data = data[2:]
+	edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+	adj = [[] for _ in range(n)]
+	for (a, b) in edges:
+		adj[a - 1].append(b - 1)
+	order = toposort(adj)
+	for x in order:
+		print(x + 1, end=' ')
 
