b283458ab5e825b869fc92542e0f08a5f76bc2de
najuzilu <junaluzi@gmail.com>
2019-10-25 01:37:15 +0200

    algo on string wk2 :rocket:

--- Algorithms_on_String/week2/assignment/suffix_array/suffix_array.py
+++ Algorithms_on_String/week2/assignment/suffix_array/suffix_array.py
@@ -1,20 +1,28 @@
 # python3
 import sys
 
+def buildSuffixArray(text):
+	"""
+	Build suffix array of the string text and
+	return a list result of the same length as the text
+	such that the value result[i] is the index (0-based)
+	in text where the i-th lexicographically smallest
+	suffix of text starts.
+	"""
+	result = []
+	textCount, mapping = {}, {}
 
-def build_suffix_array(text):
-  """
-  Build suffix array of the string text and
-  return a list result of the same length as the text
-  such that the value result[i] is the index (0-based)
-  in text where the i-th lexicographically smallest
-  suffix of text starts.
-  """
-  result = []
-  # Implement this function yourself
-  return result
+	for i in range(len(text)):
+		if text[i] not in textCount.keys():
+			textCount[text[i]] = 1
+		else:
+			textCount[text[i]] += 1
+		mapping[text[i:len(text)]] = i
 
+	for key in sorted(mapping.keys()):
+		result.append(mapping[key])
+	return result
 
 if __name__ == '__main__':
-  text = sys.stdin.readline().strip()
-  print(" ".join(map(str, build_suffix_array(text))))
+	text = sys.stdin.readline().strip()
+	print(" ".join(map(str, buildSuffixArray(text))))
