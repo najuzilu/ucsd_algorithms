fb4f4fe5bd12435b493c961c47631e1e24344b52
najuzilu <junaluzi@gmail.com>
2019-10-31 15:07:58 +0100

    algo on string wk4 assign kmp :rocket:

--- Algorithms_on_String/week4/assignment/kmp/kmp.py
+++ Algorithms_on_String/week4/assignment/kmp/kmp.py
@@ -1,21 +1,42 @@
 # python3
 import sys
 
+def compute_prefix_function(p):
+	s = [None for _ in range(len(p))]
+	s[0] = 0
+	border = 0
+
+	for i in range(1, len(p)):
+		while (border > 0) and (p[i] != p[border]):
+			border = s[border - 1]
+		if p[i] == p[border]:
+			border += 1
+		else:
+			border = 0
+		s[i] = border
+	return s
 
 def find_pattern(pattern, text):
-  """
-  Find all the occurrences of the pattern in the text
-  and return a list of all positions in the text
-  where the pattern starts in the text.
-  """
-  result = []
-  # Implement this function yourself
-  return result
+	"""
+	Find all the occurrences of the pattern in the text
+	and return a list of all positions in the text
+	where the pattern starts in the text.
+	"""
+	result = []
+	if len(pattern) > len(text):
+		return result
+
+	S = pattern + '$' + text
+	s = compute_prefix_function(S)
+
+	for i in range(len(pattern) + 1, len(S)):
+		if s[i] == len(pattern):
+			result.append(i - 2 * len(pattern))
+	return result
 
 
 if __name__ == '__main__':
-  pattern = sys.stdin.readline().strip()
-  text = sys.stdin.readline().strip()
-  result = find_pattern(pattern, text)
-  print(" ".join(map(str, result)))
-
+	pattern = sys.stdin.readline().strip()
+	text = sys.stdin.readline().strip()
+	result = find_pattern(pattern, text)
+	print(" ".join(map(str, result)))
