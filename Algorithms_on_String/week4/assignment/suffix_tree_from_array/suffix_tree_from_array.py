27d7b81d29f973038137a4f7db1150443336ec0a
najuzilu <junaluzi@gmail.com>
2019-11-13 18:03:48 +0100

    algo on string week4 assign suffix_tree_from_array breaking of node not done correctly for test case 3,4

--- Algorithms_on_String/week4/assignment/suffix_tree_from_array/suffix_tree_from_array.py
+++ Algorithms_on_String/week4/assignment/suffix_tree_from_array/suffix_tree_from_array.py
@@ -1,67 +1,109 @@
 # python3
 import sys
 
+class suffix_tree_node(object):
+	def __init__(self, children, parent, string_depth, edge_start, edge_end):
+		self.children = children
+		self.parent = parent
+		self.string_depth = string_depth
+		self.edge_start = edge_start
+		self.edge_end = edge_end
+
+def create_new_leaf(node, s, suffix):
+	"""
+		Create new leaf with the following info:
+			children = {}
+			parent = node
+			string_depth = len(s) - suffix
+			edge_start = suffix + node.string_depth
+			edge_end = len(s)
+		Update node.children[s[lead.edge_start]] = leaf
+		Return leaf
+	"""
+	leaf = suffix_tree_node({}, node, len(s) - suffix, suffix + node.string_depth, len(s))
+	node.children[s[leaf.edge_start]] = leaf
+	print("CREATE -> " ,suffix, len(s), node.string_depth)
+	return leaf
+
+def break_edge(node, s, start, offset):
+	start_char = s[start]
+	mid_char = s[start + offset]
+	# creates split node
+	mid_node = suffix_tree_node({}, node, node.string_depth + offset, start - offset - 1 + node.string_depth, start - offset + node.string_depth) # start, start + offset - 1
+
+	print("BREAK -> start=", start, " offset=", offset, " node.depth=", node.string_depth, " node.children[start_char].start=", node.children[start_char].edge_start, " node.children[start_char].end=", node.children[start_char].edge_end)
+	
+	mid_node.children[mid_char] = node.children[start_char]
+	
+	# modifies leaf which is correct
+	node.children[start_char].parent = mid_node
+	node.children[start_char].edge_start += offset
+	node.children[start_char] = mid_node
+
+	return mid_node
 
 def suffix_array_to_suffix_tree(sa, lcp, text):
-    """
-    Build suffix tree of the string text given its suffix array suffix_array
-    and LCP array lcp_array. Return the tree as a mapping from a node ID
-    to the list of all outgoing edges of the corresponding node. The edges in the
-    list must be sorted in the ascending order by the first character of the edge label.
-    Root must have node ID = 0, and all other node IDs must be different
-    nonnegative integers. Each edge must be represented by a tuple (node, start, end), where
-        * node is the node ID of the ending node of the edge
-        * start is the starting position (0-based) of the substring of text corresponding to the edge label
-        * end is the first position (0-based) after the end of the substring corresponding to the edge label
+	root = suffix_tree_node({}, None, 0, -1, -1)
+	lcp_prev = 0
+	cur_node = root
 
-    For example, if text = "ACACAA$", an edge with label "$" from root to a node with ID 1
-    must be represented by a tuple (1, 6, 7). This edge must be present in the list tree[0]
-    (corresponding to the root node), and it should be the first edge in the list (because
-    it has the smallest first character of all edges outgoing from the root).
-    """
-    tree = {}
-    # Implement this function yourself
-    return tree
+	for i in range(len(text)):
+		suffix = sa[i]
 
+		while cur_node.string_depth > lcp_prev:
+			cur_node = cur_node.parent
+
+		if cur_node.string_depth == lcp_prev:
+			print(' >')
+			cur_node = create_new_leaf(cur_node, text, suffix)
+		else:
+			print('>>')
+			edge_start = sa[i-1] + cur_node.string_depth
+			offset = lcp_prev - cur_node.string_depth
+			mid_node = break_edge(cur_node, text, edge_start, offset)
+			cur_node = create_new_leaf(mid_node, text, suffix)
+			print('mid_node -> ', mid_node.edge_start, mid_node.edge_end)
+
+		print(suffix, " -> ",cur_node.edge_start, cur_node.edge_end)
+		print("\n")
+
+		if i < len(text)-1:
+			lcp_prev = lcp[i]
+	return root
+
+def iter_tree(tree):
+	for i in range(len(tree.children)-1, -1, -1):
+		node = tree.children.values()[i]
+		print(node.edge_start, node.edge_end)
+		iter_tree(node)
 
 if __name__ == '__main__':
-    text = sys.stdin.readline().strip()
-    sa = list(map(int, sys.stdin.readline().strip().split()))
-    lcp = list(map(int, sys.stdin.readline().strip().split()))
-    print(text)
-    # Build the suffix tree and get a mapping from 
-    # suffix tree node ID to the list of outgoing Edges.
-    tree = suffix_array_to_suffix_tree(sa, lcp, text)
-    """
-    Output the edges of the suffix tree in the required order.
-    Note that we use here the contract that the root of the tree
-    will have node ID = 0 and that each vector of outgoing edges
-    will be sorted by the first character of the corresponding edge label.
-    
-    The following code avoids recursion to avoid stack overflow issues.
-    It uses two stacks to convert recursive function to a while loop.
-    This code is an equivalent of 
-    
-        OutputEdges(tree, 0);
-    
-    for the following _recursive_ function OutputEdges:
-    
-    def OutputEdges(tree, node_id):
-        edges = tree[node_id]
-        for edge in edges:
-            print("%d %d" % (edge[1], edge[2]))
-            OutputEdges(tree, edge[0]);
-    
-    """
-    stack = [(0, 0)]
-    result_edges = []
-    while len(stack) > 0:
-      (node, edge_index) = stack[-1]
-      stack.pop()
-      if not node in tree:
-        continue
-      edges = tree[node]
-      if edge_index + 1 < len(edges):
-        stack.append((node, edge_index + 1))
-      print("%d %d" % (edges[edge_index][1], edges[edge_index][2]))
-      stack.append((edges[edge_index][0], 0))
+	# Adjusted my tree printing function from recursive to 
+	# iterative to pass assignment
+	text = sys.stdin.readline().strip()
+	sa = list(map(int, sys.stdin.readline().strip().split()))
+	lcp = list(map(int, sys.stdin.readline().strip().split()))
+
+	print(text)
+
+	# Build the suffix tree and get a mapping from 
+	# suffix tree node ID to the list of outgoing Edges.
+	tree = suffix_array_to_suffix_tree(sa, lcp, text)
+
+	print("\n")
+	iter_tree(tree)
+
+	# results = []
+	# stack = [(0, 0)]
+	# result_edges = []
+
+	# while len(stack) > 0:
+	# 	(node, edge_index) = stack[-1]
+	# 	stack.pop()
+	# 	if not node in tree:
+	# 		continue
+	# 	edges = tree[node]
+	# 	if edge_index + 1 < len(edges):
+	# 		stack.append((node, edge_index + 1))
+	# 	print("%d %d" % (edges[edge_index][1], edges[edge_index][2]))
+	# 	stack.append((edges[edge_index][0], 0))
