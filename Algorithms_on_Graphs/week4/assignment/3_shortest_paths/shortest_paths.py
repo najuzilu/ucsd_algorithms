85b684c37ec93049cc86ce613e2597d6ae35bc32
najuzilu <junaluzi@gmail.com>
2019-11-14 01:26:07 +0100

    algo on graphs wk4 assign 3_shortest_paths :rocket:

--- Algorithms_on_Graphs/week4/assignment/3_shortest_paths/shortest_paths.py
+++ Algorithms_on_Graphs/week4/assignment/3_shortest_paths/shortest_paths.py
@@ -1,74 +1,55 @@
 #Uses python3
 
 import sys
-import queue
 
-def explore(v, g, visited, reach):
+def explore(v, visited):
 	visited[v] = True
-	reach[v] = 1
-	for n in g[v]:
+	reachable[v] = 1
+	for n in adj[v]:
 		if visited[n] == False:
-			explore(n, g, visited, reach)
-	return reach
+			explore(n, visited)
 
-def negative_cycle(g, c, distance, s):
-	L = []
+def negative_cycle(s):
+	Q = []
 	distance[s] = 0
-	for iter_ in range(len(g)):
-		for i in range(len(g)):
-			for j in range(len(g[i])):
-				nodeVal = g[i][j]
-				costVal = c[i][j]
+	for iter_ in range(len(adj)):
+		for i in range(len(adj)):
+			for j in range(len(adj[i])):
+				nodeVal = adj[i][j]
+				costVal = cost[i][j]
 				if distance[nodeVal] > distance[i] + costVal:
-					distance[nodeVal] = distance[i] + costVal
-					### if in last iteration of verticies, 
-					### then any negative node cycles append to heapq
-					if iter_ == len(g) - 1:
-						L.append(nodeVal)
-	return L, distance
+					if iter_ == len(adj) - 1:
+						distance[nodeVal] = - float("inf")
+						Q.append(nodeVal)
+					else:
+						distance[nodeVal] = distance[i] + costVal
+	return Q
 
-def exploreNeg(v, g, visited):
+def explore_negative(v, visited):
 	visited[v] = True
-	shortest[v] = 0
-	distance[v] = - float("inf")
-	for n in g[v]:
-		# print('v={}, shortest[v]={}, distance[v]={}, shortest[n]={}, distance[n]={}'.format(v, shortest[v], distance[v], shortest[n], distance[n]))
+	for n in adj[v]:
 		if visited[n] == False:
-			exploreNeg(n, g, visited)
+			shortest[n] = 0
+			explore_negative(n, visited)
 
 def shortet_paths(adj, cost, s, distance, reachable, shortest):
-	### Step 1: vector named component to restore all connected
-	### vertices explored from source s, and a function to get 
-	### these elements. This vector contains the order information, 
-	### which is significant for next moves.
+	# Find negative cycles and map as -inf
+	Q = negative_cycle(s)
 
-	visited = [False for _ in range(len(adj))]
-	reachable = explore(s, adj, visited, reachable)
-
-	### Step 2: Do V-1 times B-F algorithm, from now on, there 
-	### will be some changes if one vertex is in a negative cycle.
-	Q, distance = negative_cycle(adj, cost, distance, s)
-
-	### Step 3: 2 iterations based on the component order, 
-	### look up all vertices in component, do relax function, 
-	### but this time let the distance equals to -INF
-
-	visitedNeg = [False for _ in range(len(adj))]
+	# Convert all neg infinite values to shortest = 0
 	for q in Q:
-		if visitedNeg[q] == False:
-			exploreNeg(q, adj, visitedNeg)
-
+		shortest[q] = 0
+		visited_negative = [False for _ in range(len(adj))]
+		explore_negative(q, visited_negative)
 	distance[s] = 0
-	# while Q:
-	# 	dequeue_node = Q.pop(0)
-	# 	shortest[dequeue_node] = 0
-	# 	for i in range(len(adj[dequeue_node])):
-	# 		child = adj[dequeue_node][i]
-	# 		if distance[child] > distance[dequeue_node] + cost[dequeue_node][i]:
-	# 			distance[child] = -(float("inf"))
-	# 			# shortest[child] = 0
-	# 			Q.append(child)
 
+	# Convert all non inifnite values to reachable = 1
+	for i in range(len(distance)):
+		item = distance[i]
+		if item == float("inf"):
+			reachable[i] = 0
+		else:
+			reachable[i] = 1
 
 if __name__ == '__main__':
 	input = sys.stdin.read()
@@ -84,7 +65,6 @@ if __name__ == '__main__':
 		cost[a - 1].append(w)
 	s = data[0]
 	s -= 1
-	# distance = [10**19] * n
 	distance = [float("inf")] * n
 	reachable = [0] * n
 	shortest = [1] * n
