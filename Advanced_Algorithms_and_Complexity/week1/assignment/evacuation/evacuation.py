19bbe904150e005b96768e33c43a533c75eea51c
najuzilu <junaluzi@gmail.com>
2019-12-28 16:40:41 +0100

    advanced algo wk1 assign1 implemented shortest path algo; left to do: compute G_f and implement Ford-Fulkerson algorithm

--- Advanced_Algorithms_and_Complexity/week1/assignment/evacuation/evacuation.py
+++ Advanced_Algorithms_and_Complexity/week1/assignment/evacuation/evacuation.py
@@ -1,4 +1,6 @@
 # python3
+import queue
+import sys
 
 class Edge:
 
@@ -16,7 +18,7 @@ class FlowGraph:
         # List of all - forward and backward - edges
         self.edges = []
         # These adjacency lists store only indices of edges in the edges list
-        self.graph = [[] for _ in range(n)]
+        self.graph = [[] for _ in range(n)] # keeps track of len of ingoing and outgoing edges per node
 
     def add_edge(self, from_, to, capacity):
         # Note that we first append a forward edge and then a backward edge,
@@ -48,7 +50,6 @@ class FlowGraph:
         self.edges[id].flow += flow
         self.edges[id ^ 1].flow -= flow
 
-
 def read_data():
     vertex_count, edge_count = map(int, input().split())
     graph = FlowGraph(vertex_count)
@@ -57,10 +58,70 @@ def read_data():
         graph.add_edge(u - 1, v - 1, capacity)
     return graph
 
+def shortest_path_distance(graph, s, t):
+    # Initialize queue
+    L = queue.Queue(maxsize = len(graph.edges))
+    # Initialize distance list
+    distance = [len(graph.edges) for _ in range(len(graph.edges))]
+    # Initialize previous to nil
+    previous = [None for _ in range(len(graph.edges))]
+    # Set starting node distance to 0
+    distance[s] = 0
+    # Add starting node to queue
+    L.put(s)
+
+    # While queue is not empty
+    while not L.empty():
+        # Take the first element
+        dequeue_node = L.get()
+        for node in graph.get_ids(dequeue_node):
+            edge = graph.get_edge(node)
+            if distance[edge.v] == len(graph.edges):
+                L.put(edge.v)
+                distance[edge.v] = distance[dequeue_node] + 1
+                previous[edge.v] = dequeue_node
+    return distance, previous
+
+def deconstruct_path(from_, to, distance, previous, graph):
+    """
+        Deconstructs path and returns str of path and 
+    """
+    path = [to]
+    copy_to = to
+    min_distance = []
+
+    while copy_to:
+        min_distance.append(graph.get_edge(previous[copy_to]).flow)
+        if copy_to == from_:
+            path = [previous[copy_to]] + path
+            break
+
+        path = [previous[copy_to]] + path
+        copy_to = previous[copy_to]
+
+    return path, min(min_distance)
+
 
 def max_flow(graph, from_, to):
     flow = 0
-    # your code goes here
+    print('from = {}, to = {}, length of graph.edges={}'.format(from_, to, len(graph.edges)))
+
+    if len(graph.edges) == 0:
+        return 0
+
+    # Have successfully computed shortest path
+    distance, previous = shortest_path_distance(graph, from_, to)
+    print(">>>", deconstruct_path(from_, to, distance, previous, graph))
+
+    # # # Basic idea of Ford-Fulkerson Algorithm
+    # # while True:
+    # #     compute G_f
+    # #     find shortest path from_->to path P in G_f
+    # #     if no path: return flow
+    # #     X -> min edge in shortest path
+    # #     g flow with g_e = X for edge in P
+    # #     f <- f + g
+
     return flow
 
 
